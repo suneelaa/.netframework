ee7dae9c5ca03c6a5eab9329514c6d883755d30e
suneelaa <105145781+suneelaa@users.noreply.github.com>
2022-07-29 14:48:37 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,62 @@
+﻿using JokeWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokeWebApplication.Controllers
+{
+    public class Product
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string price { get; set; }
+        public string quantity { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public List<Product> products()
+        {
+            var products = new List<Product>
+            {
+                new Product{
+                    id = "1",
+                    name = "Tea",
+                    price = "99.99",
+                    quantity = "5"
+
+                }
+            };
+
+             return products;
+        }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // https://localhost:44301/home/privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // https://localhost:44301/home/error
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TemperatureController.cs
@@ -0,0 +1,101 @@
+﻿using JokeWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokeWebApplication.Controllers
+{
+    public class TemperatureController : Controller
+    {
+        // /temperature/getTemperature
+        public string GetTemperature()
+        {
+            string temp = "23°C";
+
+            return temp;
+        }
+
+        public int GetMaxTemperature()
+        {
+            return 50;
+        }
+
+        public Joke GetAJoke()
+        {
+            var joke = new Joke
+            {
+                Id = 1,
+                JokeQuestion = "What is ASP?",
+                JokeAnswer = "Active Server Pages"
+            };
+
+            return joke;
+        }
+        public Joke GetJokeByID([FromQuery] int id)
+        {
+            Joke result = null;
+
+            List<Joke> allJokes = new List<Joke>();
+            var joke = new Joke
+            {
+                Id = 1,
+                JokeQuestion = "What is ASP?",
+                JokeAnswer = "Active Server Pages"
+            };
+
+            var joke2 = new Joke
+            {
+                Id = 2,
+                JokeQuestion = "What is JS?",
+                JokeAnswer = "Jacascript"
+            };
+
+            allJokes.Add(joke);
+            allJokes.Add(joke2);
+
+            result = allJokes.FirstOrDefault(j => j.Id == id);
+
+            return result;
+        }
+
+        public List<Joke> GetAllJokes()
+        {
+            List<Joke> allJokes = new List<Joke>();
+            var joke = new Joke
+            {
+                Id = 1,
+                JokeQuestion = "What is ASP?",
+                JokeAnswer = "Active Server Pages"
+            };
+
+            var joke2 = new Joke
+            {
+                Id = 2,
+                JokeQuestion = "What is JS?",
+                JokeAnswer = "Jacascript"
+            };
+
+            allJokes.Add(joke);
+            allJokes.Add(joke2);
+
+            return allJokes;
+        }
+
+        [HttpPost]
+        public object CreateAJoke([FromBody] Joke joke)
+        {
+            var myJoke = new Joke
+            {
+                Id = joke.Id,
+                JokeQuestion = joke.JokeQuestion,
+                JokeAnswer = joke.JokeAnswer
+            };
+
+            return new { status ="OK", message="Received your joke", joke = myJoke };
+        }
+    }
+}
